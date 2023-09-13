dc86afb07715167f81d22cefb1728eb62c902758
Nayrb29 <bryan201429@gmail.com>
2023-09-12 23:28:44 -0500

    Cloudflare ready

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,8 +8,8 @@ export default function Footer(){
             <div id='logoBar'><img src={realBlack} className='logoBlack'></img></div>
             <div id='info'>
 
-                <div id='Copyright'>2023 Sauna Spa Real Paradise</div>
-                <div id='Developer'>Develoved by:
+                <div id='Copyright'>2023 Sauna Spa Real Paradise®</div>
+                <div id='Developer'>Developed by:
                 <a id='user' href='https://github.com/bryan201429'>brn201429 
                     <img className='githubLogo' src={githubLogo}></img>
                 </a>
