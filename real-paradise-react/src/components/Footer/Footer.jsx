550bb1011755ca5351a35d1f6a2898cbc1366aa6
Nayrb29 <bryan201429@gmail.com>
2023-09-12 22:16:55 -0500

    Build 1

--- real-paradise-react/src/components/Footer/Footer.jsx
+++ real-paradise-react/src/components/Footer/Footer.jsx
@@ -1,9 +1,21 @@
 import './Footer.css'
+import logoBlack from '../../assets/logoBlack.png'
+import githubLogo from '../../assets/githubLogoWhite.png'
 
 export default function Footer(){
     return(
-        <div id='FooterContainer'>
-            
+        <div id='Container'>
+            <div id='logoBar'><img src={logoBlack} className='logoBlack'></img></div>
+            <div id='info'>
+
+                <div id='Copyright'>2023 Sauna Spa Real Paradise</div>
+                <div id='Developer'>Develoved by:
+                <a id='user' href='https://github.com/bryan201429'>brn201429 
+                    <img className='githubLogo' src={githubLogo}></img>
+                </a>
+                </div>
+                
+            </div>
         </div>  
     )
 }
---
+++ real-paradise-react/src/components/Footer/FooterInfo.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import logoBlack from '../../assets/logoBlack.png'
+
+export default function Footer(){
+    return(
+        <div id='FooterContainer'>
+        <h2>Av. Dolores 132</h2>
+    </div>
+    )
+}
