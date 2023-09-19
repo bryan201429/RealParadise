eaf86839e0a95373ad517e2a790507d9808eac69
Nayrb29 <bryan201429@gmail.com>
2023-09-19 11:56:16 -0500

    Responsive slider

--- src/components/detailBox/DetailBox.jsx
+++ src/components/detailBox/DetailBox.jsx
@@ -21,14 +21,14 @@ export default function DetailBox(){
                 <hr className="linea-horizontal2"></hr>
                 <div className='horariosContainer'>
                     <div className='horariosDias'>
-                        <h2>Lunes</h2>
-                        <h2>Martes</h2>
-                        <h2>Miercoles</h2>
-                        <h2>Jueves</h2>
-                        <h2>Viernes</h2>
-                        <h2>Sábado</h2>
-                        <h2>Domingo</h2>
-                        <h2>Feriados</h2>
+                        <h2>LUNES</h2>
+                        <h2>MARTES</h2>
+                        <h2>MIERCOLES</h2>
+                        <h2>JUEVES</h2>
+                        <h2>VIERNES</h2>
+                        <h2>SABADO</h2>
+                        <h2>DOMINGO</h2>
+                        <h2>FERIADOS</h2>
                     </div>
                     <div className='horariosHoras'>
                         <h2>9:00 AM - 9:00 PM</h2>
